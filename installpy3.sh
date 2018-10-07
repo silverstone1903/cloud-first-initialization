bba0289142c01a7682cf446cce99fef257c024ae
silverstone1903 <silverstone1903@users.noreply.github.com>
2018-10-07 20:08:55 +0300

    Python3

---
+++ installpy3.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get -y upgrade
+
+sudo apt-get install git
+sudo apt-get install -y python3-pip
+sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+
+sudo -H pip3 install scipy scikit-learn matplotlib jupyter pandas numpy tensorflow keras seaborn xgboost catboost keras kaggle
+sudo -H pip3 install lightgbm
