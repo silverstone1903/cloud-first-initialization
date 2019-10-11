d12823a2a2898d73058eab4b1c42c9f26f70c737
silverstone <silverstone@ss.com>
2019-10-11 11:28:55 +0300

    install bash echo messages added.

--- ml-env/StartDocker.sh
+++ ml-env/StartDocker.sh
@@ -1,4 +1,8 @@
+echo Update & Upgrade
+
 sudo apt-get update && sudo apt-get upgrade -y
+
+echo Prerequisites for Docker
 
 sudo apt-get install \
     apt-transport-https \
@@ -18,9 +22,15 @@ sudo add-apt-repository \
 
 sudo apt-get update
 
+echo Installing Docker
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
 
+echo Hello World
+
 sudo docker run hello-world
+
+echo Pulling ml-envs
 
 sudo docker pull -a silverstone1903/ml-env
 
