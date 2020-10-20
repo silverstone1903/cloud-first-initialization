ad94c72f118838f80ee4453ff6202e4ddd875e93
silverstone <silverstone@ss.com>
2020-10-20 14:05:39 +0300

    dockerfile update

--- pythonr/base/rpackages_installer.R
+++ pythonr/base/rpackages_installer.R
@@ -3,4 +3,5 @@ if (!requireNamespace("data.table")) install.packages("data.table", type = "sour
 if (!requireNamespace("DT")) install.packages("DT", quiet = T)
 if (!requireNamespace("magrittr")) install.packages("magrittr", quiet = T)
 if (!requireNamespace("tidyverse")) install.packages("tidyverse", quiet = T)
-if (!requireNamespace("hrbrthemes")) install.packages("hrbrthemes", repos = c("https://cinc.rud.is", "https://cloud.r-project.org/", quiet = T)
+if (!requireNamespace("hrbrthemes")) install.packages("hrbrthemes", 
+	repos = c("https://cinc.rud.is", "https://cloud.r-project.org/"), quiet = T)
